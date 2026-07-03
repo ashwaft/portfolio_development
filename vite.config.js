6bf6e311c629d3761ef103cbb8fb54b897d2f08a
Tengku Asyraf (RP) <tengku_asyraf@rp.edu.sg>
2026-07-03 10:36:05 +0800

    update config

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/portfolio_development/", // Replace with your GitHub repository name
+  base: "/", // Replace with your GitHub repository name
 });
